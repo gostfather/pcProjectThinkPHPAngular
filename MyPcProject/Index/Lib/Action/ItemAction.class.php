672296e8f9b4a8965172760c15ce7a4a54b51d53
857902620@qq.com <857902620@qq.com>
2017-08-08 22:09:22 +0800

    sjy

--- MyPcProject/Index/Lib/Action/ItemAction.class.php
+++ MyPcProject/Index/Lib/Action/ItemAction.class.php
@@ -1,11 +1,12 @@
 <?php
-	class ItemAction extends Action {
-  	public function item(){
-      $classify = I("item");
-      $Item = D("Item");
-      $res = $Item -> getList($classify);
-      $this -> assign("classify",json_encode($res));
-      $this->display("item");
-    }
+class ItemAction extends Action {
+	public function item(){
+		$classify = I("item");
+		$Item = D("Item");
+		$res = $Item -> getList($classify);
+		$this -> assign("classify",json_encode($res));
+		$this->display("item");
+	}
+	
 }
 ?>
